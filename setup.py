2b11be5c42451e203cc8f945f8cc79ae1d3cf6f8
FrancescoSaverioZuppichini <francesco.zuppichini@gmail.com>
2022-10-14 10:48:00 +0200

    minor changes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="template-python-zuppif",  
-    version="0.0.1",
+    version="0.0.2",
     author="zuppif",
     author_email="francesco.zuppichini@gmail.com",
     description="<INSERT_DESCRIPTION>",
